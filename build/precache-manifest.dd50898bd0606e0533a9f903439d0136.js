a40f16be45b95ed6f377d724708c6705364149c5
Taufiq Effendi <taufiqeffendi66@gmail.com>
2021-08-30 15:18:19 +0700

    first commit

---
+++ build/precache-manifest.dd50898bd0606e0533a9f903439d0136.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b7baebec50f37d0d218",
+    "url": "/resume/static/css/main.1a1ae0e6.chunk.css"
+  },
+  {
+    "revision": "0b7baebec50f37d0d218",
+    "url": "/resume/static/js/main.0b7baebe.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/resume/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "18bea22a2a5d1e1d102c",
+    "url": "/resume/static/js/runtime~main.18bea22a.js"
+  },
+  {
+    "revision": "b891127d98eed4c8f58ad992fefb828d",
+    "url": "/resume/index.html"
+  }
+];
---
+++ src/resumeData.js
@@ -0,0 +1,98 @@
+let resumeData = {
+    // "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Taufiq Maulana Effendi",
+    "role": "Backend and Frontend Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like to be involved in technology. My daily life besides learn about new technologies, watching movies or playing games.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/rbhatia46",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/rbhatia46",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am a graduate of Universitas Pembangunan Nasional Veteran Jakarta. I currently work as a fullstack developer. Until now I have always developed knowledge of technology, especially in web developers. Man Jadda Wajada, I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Jakarta, Indonesia",
+    "email":"taufiqeffendi66@gmail.com",
+    "website":"https://fendi66.github.io",
+    "education":[
+      {
+        "UniversityName":"Universitas Pembangunan Nasional Veteran Jakarta",
+        "specialization":"Informatics Engineering",
+        "MonthOfPassing":"Oct",
+        "YearOfPassing":"2017",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"PT Bank UOB Indonesia",
+        "specialization":"Data Management Staff",
+        "MonthOfLeaving":"May",
+        "YearOfLeaving":"2019"
+      },
+      {
+        "CompanyName":"PT Codevergence",
+        "specialization":"Software Engineering",
+        "MonthOfLeaving":"-",
+        "YearOfLeaving":"-"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"jquery"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
